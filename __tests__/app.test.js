98cf37c16420cd19ede09abfa82a0899c55278fe
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-15 17:45:54 -0700

    got my test to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,11 +11,24 @@ describe('alchemy-app routes', () => {
   afterAll(() => {
     pool.end();
   });
-});
 
-it('gets list of phantom troupe members', async () => {
-  const expected = [{ id: 1, name: 'Chrollo', number: 0 }];
-  const res = await request(app).post('/api/v1/members').send(expected);
+  it('creates a member', async () => {
+    const expected = {
+      name: 'Shizuku',
+      number: 8,
+    };
+    const res = await request(app).post('/api/v1/members').send(expected);
 
-  expected(res.body).toEqual(expected);
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  // it('gets list of phantom troupe members', async () => {
+  //   const expected = [
+  //     { id: 1, name: 'Chrollo', number: 0 },
+  //     { id: 2, name: 'Feitan', number: 2 },
+  //   ];
+  //   const res = await request(app).get('/api/v1/members').send(expected);
+
+  //   expect(res.body).toEqual(expected);
+  // });
 });
