33707eabcea03853551fbf4f47aaae15f3c9f8fb
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-16 16:47:18 -0700

    got my update test and get member by Id to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -30,6 +30,32 @@ describe('alchemy-app routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+  it('gets a member by id', async () => {
+    const expected = await Member.findById(1);
+    const res = await request(app).get(`/api/v1/members/${expected.id}`);
+
+    expect(res.body).toEqual({ ...expected });
+  });
+
+  it('updates a member by id', async () => {
+    const initial = {
+      id: expect.any(String),
+      name: 'Chrollo',
+      number: 1,
+    };
+    const troupe = await request(app).post('/api/v1/members').send(initial);
+    const expected = {
+      id: expect.any(String),
+      name: 'Chrollo',
+      number: 0,
+    };
+    const res = await request(app)
+      .patch(`/api/v1/members/${troupe.body.id}`)
+      .send({ number: 0 });
+
+    expect(res.body).toEqual(expected);
+  });
+
   // it('gets list of phantom troupe members', async () => {
   //   const expected = [
   //     { id: 1, name: 'Chrollo', number: 0 },
